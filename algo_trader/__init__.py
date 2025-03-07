79cc0fd5e2967589c5bb660413618eaf1549075d
tim-tanner <tim.tanner@tordivel.no>
2025-03-07 22:31:07 +0000

    first commit

---
+++ algo_trader/__init__.py
@@ -0,0 +1,19 @@
+"""
+===================================================
+Script Name:       __init__.py
+Author:            Tim Tanner, tim.tanner@gmail.com
+Description:       algo_trader
+Version:           1.0.0
+Release History:
+    07mar2025: 1.0.0 - Initial release
+
+Created on:        07mar2025
+===================================================
+"""
+
+__author__ = "Tim Tanner, tim.tanner@gmail.com"
+__version__ = "1.0.0"
+__date__ = "07mar2025"
+__license__ = "MIT"
+
+# Your code starts here...
---
+++ examples/load_data.py
@@ -0,0 +1,19 @@
+"""
+===================================================
+Script Name:       load_data.py
+Author:            Tim Tanner, tim.tanner@gmail.com
+Description:       algo trader test for loading data
+Version:           1.0.0
+Release History:
+    07mar2025: 1.0.0 - Initial release
+
+Created on:        07mar2025
+===================================================
+"""
+
+__author__ = "Tim Tanner, tim.tanner@gmail.com"
+__version__ = "1.0.0"
+__date__ = "07"
+__license__ = "MIT"
+
+# Your code starts here...
